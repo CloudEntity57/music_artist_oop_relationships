0d271b98394bd1cf7ecda17f125885d9fb80bdb9
Josh Foster <josh@allenb.com>
2016-12-01 19:54:23 -0600

    created both constructors and artist object seemed to have new song pushed into its song list

--- music.js
+++ music.js
@@ -1,13 +1,21 @@
 // Define the Artist constructor function here, with a 'name' property that can be set at instantiation,
 // and a 'songs' property that is an empty array to start
-function Artist() {
-
+function Artist(name, songs) {
+  this.name = name;
+  this.songs = songs || [];
 }
 
 // Define the Song constructor function here, with 'title' and 'artist' properties that can be set at instantiation,
 // and a 'playCount' property that is set to 0
-function Song() {
-
+var playCount = 0;
+function Song(title,artist) {
+  this.title = title;
+  this.artist = artist;
+  this.playCount = 0;
+  this.artist.songs.push(this);
+  this.play = function(){
+    return this.playCount++;
+  };
   // When a new song is instantiated, you'll want to add that song to its artist's 'songs' array.
   // You'll probably want to do that in here somewhere...
 }
@@ -15,3 +23,7 @@ function Song() {
 // This is how we should be able to use your constructor functions once they work!
 var vanHalen = new Artist("Van Halen");
 var jump = new Song("Jump!", vanHalen);
+console.log('artist is: ',vanHalen.songs,'!!!!!');
+jump.play();
+jump.play();
+console.log(jump);
